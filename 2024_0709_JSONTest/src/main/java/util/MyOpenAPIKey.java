1524dfc424bf189a8e84c9af4ce93098289be402
차선일 <cktjsdlf4636@naver.com>
2024-07-09 15:26:01 +0900

    [ADD] 오늘의 수업내용

---
+++ 2024_0709_JSONTest/src/main/java/util/MyOpenAPIKey.java
@@ -0,0 +1,19 @@
+package util;
+
+public class MyOpenAPIKey {
+	
+	// 여러개의 키 값을 변수로 저장해놓는것
+	public static class Kakao{
+		public static final String API_Key = "KakaoAK a8ad36db49c640c1deba7011ff4cc7be";
+	}
+	
+	public static class Naver{
+		public static final String CLIENT_ID = "";
+		public static final String CLIENT_SECRET = "";
+	}
+	
+	public static class Google{
+		
+	}
+
+}
