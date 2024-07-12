421d9f73b094574e0aeb3157e0c7a5dafcb54cc0
차선일 <cktjsdlf4636@naver.com>
2024-07-12 10:10:11 +0900

    [ADD] 페이징

---
+++ 2024_0710_PhotoGallery_Mybatis/imageSync.bat
@@ -0,0 +1,3 @@
+del  D:\dev\WebStudy\2024_0710_PhotoGallery_Mybatis\src\main\webapp\images\*.*
+
+copy D:\dev\WebStudy\.metadata\.plugins\org.eclipse.wst.server.core\tmp1\wtpwebapps\2024_0710_PhotoGallery_Mybatis\images\*.*  D:\dev\WebStudy\2024_0710_PhotoGallery_Mybatis\src\main\webapp\images
