0077a10ee6e98d5cba53c3dc0679f3727916bc03
차선일 <cktjsdlf4636@naver.com>
2024-07-19 11:52:16 +0900

    [ADD] Spring DB Visit

--- 2024_0718_SpringVisit/src/main/java/controller/VisitController.java
+++ 2024_0718_SpringVisit/src/main/java/controller/VisitController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.util.HashMap;
-
 import java.util.List;
 import java.util.Map;
 
@@ -10,8 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import action.util.MyCommon;
 import action.util.Paging;
@@ -21,6 +23,13 @@ import vo.VisitVo;
 @Controller
 public class VisitController {
 
+	// @RequestMapping에 의해서 Method 호출시 DS가 자동으로 Injection(주입)시켜준다.
+	// 해당컨트롤러가 Auto-Detecting으로 생성시에만 넣어준다.
+	// @Controller로 자동생성 되어있어서 무조건 생성 됨.
+	// 수동생성시에는 생성전에 <context:annotation-config/> 등록해야 한다.
+	@Autowired
+	HttpServletRequest request;
+	
 	public VisitController() {
 		System.out.println("--VisitController()--");
 	}
@@ -29,28 +38,30 @@ public class VisitController {
 	// 자동엮기(자동으로 injection)
 	@Autowired
 	VisitDao visit_dao;
-	
-	// 전체조회
+	// (전체+필터)조회
+	// /visit/list.do
+	// /visit/list.do?page=2&search=name_content&search_text=현진
 	@RequestMapping("/visit/list.do")
-	public String list(HttpServletRequest request, HttpServletResponse response) {
+	public String list(@RequestParam(name="search", defaultValue = "all") String search, 
+					   String search_text,
+					   @RequestParam(name="page", defaultValue = "1") int nowPage,
+					   Model model) {
 		
+		// 기존에는 Method 인자를 던져주고 Spring이 request parameter를 받아줬던 것이다.
 		// 1. parameter 받기
-		String search		=		request.getParameter("search");
-		String search_text	=		request.getParameter("search_text");
+		// String search		=		request.getParameter("search");
+		//  search_text	=		request.getParameter("search_text");
 		
 		// search 값이 안들어왔을때 그냥 default로 전체보기로 보여주겠다.(안넣어주면 nullPointException)
-		if(search==null) {
-			search="all";
-		}
+		/*
+		   if(search==null) { search="all"; }
+		 */
 		
-		
-		
-		int nowPage = 1;
-		try {
-			nowPage = Integer.parseInt(request.getParameter("page"));
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+		/*
+		 * int nowPage = 1; try { nowPage =
+		 * Integer.parseInt(request.getParameter("page")); } catch (Exception e) { //
+		 * TODO: handle exception }
+		 */
 		
 		// start/end
 		int BLOCK_LIST = 2;
@@ -82,7 +93,7 @@ public class VisitController {
 		// 전체 게시물수
 		int rowTotal = visit_dao.selectRowTotal(map);
 		
-		// 검색정보 filter : search_filter = "search=name&search_text=길동"
+		// 검색정보 filter : search_filter = "search=name&search_text=길동"0000
 		String search_filter = String.format("search=%s&search_text=%s", search, search_text);
 		
 		// pageMenu 만들기
@@ -94,8 +105,11 @@ public class VisitController {
 											MyCommon.Visit.BLOCK_PAGE); // 한화면에 보여질 페이지 수
 		
 		// request binding
-		request.setAttribute("list", list);
-		request.setAttribute("pageMenu", pageMenu);
+		// request.setAttribute("list", list);
+		// request.setAttribute("pageMenu", pageMenu);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("pageMenu", pageMenu);
 		
 
 		return "visit/visit_list"; // /WEB-INF/views/ + visit/visit_list + .jsp
@@ -108,28 +122,38 @@ public class VisitController {
 	
 	// 입력폼 띄우기
 	@RequestMapping("/visit/insert_form.do")
-	public String insert_form(HttpServletRequest request, HttpServletResponse response) {
+	public String insert_form() {
 		
-		return "visit_insert_form.jsp";
+		return "visit/visit_insert_form"; // viewName
 	}// end:insert_form()
 	
 	
 	
 	// 입력(등록)하기
+	// /visit/insert.do?name=홍길동&content=내용&pwd=1234
 	@RequestMapping("/visit/insert.do")
-	public String insert(HttpServletRequest request, HttpServletResponse response) {
+	public String insert(VisitVo vo) {
 	
+		// Method인자 : DispatcherServlert에 대한 요구사항
+		// 나 호출할 때 (invokation) parameter 들어올 텐데, 그걸 vo로 포장해서 줘
+		
 		// 1. parameter(전달인자) 받기
+		/*
 		String name = request.getParameter("name");
 		String content = request.getParameter("content").replaceAll("\n", "<br>");
 		String pwd = request.getParameter("pwd");
+		*/
+		
+		String content = vo.getContent().replaceAll("\n", "<br>");
+		vo.setContent(content);
 		
 		// 2. ip 정보 받기
 		String ip = request.getRemoteAddr();
+		vo.setIp(ip);
 		
 		// 3. VisitVo 포장
 		// vo에서 오버로드 된 생성자 새로 추가
-		VisitVo vo = new VisitVo(name, content, pwd, ip);
+		// VisitVo vo = new VisitVo(name, content, pwd, ip);
 		
 		// 4. DBinsert
 		int res = visit_dao.insert(vo);
@@ -138,19 +162,26 @@ public class VisitController {
 		// 내가 원하는것은 Redirect이다.
 		// return "visit_list.jsp"; (기존 Dispatcher 형식)
 		return "redirect:list.do";
-		// FrontController에게 반환
+		// FrontController(현재:DispatcherServlet)에게 반환
+		// redirect : 접두어 확인 후 response.sendRedirect("list.do") 처리
 	}// end:insert
 	
 	
 	
+	
+	
+	
+	
+	// /visit/check_pwd.do?idx=5&c_pwd=1234
 	// 입력(등록)하기
 	@RequestMapping(value="/visit/check_pwd.do",produces="application/json;charset=utf-8")
+	// view가 아니라 응답데이터로 넘겨줄께
 	@ResponseBody
-	public String check_pwd(HttpServletRequest request, HttpServletResponse response) {
+	public String check_pwd(int idx,String c_pwd) {
 		
 		// parameter 받기
-		int idx = Integer.parseInt(request.getParameter("idx"));
-		String c_pwd = request.getParameter("c_pwd");
+		// int idx = Integer.parseInt(request.getParameter("idx"));
+		// String c_pwd = request.getParameter("c_pwd");
 		
 		// idx에 해당되는 게시물 1건을 얻어온다
 		VisitVo vo = visit_dao.selectOne(idx);
@@ -161,15 +192,23 @@ public class VisitController {
 		// JSON Data 생성 전송 : {"result":true}
 		String json = String.format("{\"result\":%b}", bResult);
 		
+		// @ResponseBody
+		// 반환 값을 DispatcherServlet이 직접 전송
+		
+		// contentType은 @RequestMappong에 produces 값을 사용한다.
+		/*
+		 	response.setContentType("application/json;charset=utf-8");
+		 	response.getWriter().print();
+		 */
+		
 		return json; // 이렇게 사용할시 forward 되어버린다 jsp가 아니기 때문에 505 error
 	}// end:check_pwd
 	
 	// 삭제하기
 	@RequestMapping("/visit/delete.do")
-	public String delete(HttpServletRequest request, HttpServletResponse response) {
+	public String delete(int idx) {
 		
-		int idx = Integer.parseInt(request.getParameter("idx"));
-		String no = request.getParameter("no"); // 삭제할 글의 순서
+		// int idx = Integer.parseInt(request.getParameter("idx"));
 		
 		// DB 삭제
 		int res = visit_dao.delete(idx);
@@ -178,10 +217,11 @@ public class VisitController {
 	}// end:delete
 	
 	// 수정폼으로 연결
+	// /visit/modify_form.do?idx=5
 	@RequestMapping("/visit/modify_form.do")
-	public String modify_form(HttpServletRequest request, HttpServletResponse response) {
+	public String modify_form(int idx, Model model) {
 		
-		int idx = Integer.parseInt(request.getParameter("idx"));
+		// int idx = Integer.parseInt(request.getParameter("idx"));
 		
 		// idx에 해당되는 게시물 1건 얻어오기
 		VisitVo vo = visit_dao.selectOne(idx);
@@ -191,35 +231,59 @@ public class VisitController {
 		vo.setContent(content);
 
 		// request binding(JSP에게 vo의 데이터를 넘겨줘야 하기 때문에 setAttribute 한다)
-		request.setAttribute("vo", vo);
+		model.addAttribute("vo", vo);
 		
-		return "visit_modify_form.do";
+		return "visit/visit_modify_form";
 	}// end:modify_form
+	
+	
+	
+	
 	
 	// 수정하기
 	@RequestMapping("/visit/modify.do")
-	public String modify(HttpServletRequest request, HttpServletResponse response) {
+	public String modify(VisitVo vo,
+						@RequestParam(name="page",defaultValue = "1") int page,
+						@RequestParam(name="search",defaultValue="all") String search,
+						String search_text,
+						RedirectAttributes ra
+			) {
 		
 		// parameter 받기
-		int idx = Integer.parseInt(request.getParameter("idx"));
+		// int idx = Integer.parseInt(request.getParameter("idx"));
 		// String no = request.getParameter("no");
-		String name = request.getParameter("name");
-		String content = request.getParameter("content").replaceAll("\n", "<br>");
-		String pwd = request.getParameter("pwd");
+		// String name = request.getParameter("name");
+		// String content = request.getParameter("content").replaceAll("\n", "<br>");
+		// String pwd = request.getParameter("pwd");
+
+		
+		String content = vo.getContent().replaceAll("\n", "<br>");
+		vo.setContent(content);
 		
 		
-		String page = request.getParameter("page");
-		String search = request.getParameter("search");
-		String search_text = request.getParameter("search_text");
+		// String page = request.getParameter("page");
+		// String search = request.getParameter("search");
+		// String search_text = request.getParameter("search_text");
+		
 		
 		// ip주소 얻어온다
 		String ip = request.getRemoteAddr();
+		vo.setIp(ip);
 		
 		// VisitVo 포장
-		VisitVo vo = new VisitVo(idx, name, content, pwd, ip);
+		// VisitVo vo = new VisitVo(idx, name, content, pwd, ip);
 		
 		// DB 수정하기
 		int res = visit_dao.update(vo);
+		
+		// RedirectAttributes : redirect parameter 정보담는객체
+		ra.addAttribute("page", page);
+		ra.addAttribute("search",search);
+		ra.addAttribute("search_text",search_text);
+		
+		
+		// DispatcherServlet이 response.sendRedirect("page=%s&search=name&search_text=길동");
+		
 		
 		return "redirect:list.do";
 	}// end:modify
