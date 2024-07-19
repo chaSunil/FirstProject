81a5be706d1fa55fabe4b3519d7a52413579582b
차선일 <cktjsdlf4636@naver.com>
2024-07-19 17:47:45 +0900

    [ADD] Spring DB FileUpload 20240719

---
+++ 2024_0719_SpringFileUpload/src/main/java/vo/PhotoVo.java
@@ -0,0 +1,30 @@
+package vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class PhotoVo {
+	
+	String title;
+	String filename;
+	MultipartFile photo;
+	
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public MultipartFile getPhoto() {
+		return photo;
+	}
+	public void setPhoto(MultipartFile photo) {
+		this.photo = photo;
+	}
+
+}
